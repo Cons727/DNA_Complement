ebd69e8eee235317ce23ebe707d4a1930313a798
Cons727 <const_marini@hotmail.com>
2022-10-24 00:58:34 +0000

    Added help info

--- app/ui.R
+++ app/ui.R
@@ -5,7 +5,8 @@ fluidPage(
   sidebarLayout(
     sidebarPanel(
       helpText("This program returns the complementary sequence of a DNA sequence. 
-               Input box can be resized for better visualization by dragging the lower right corner."),
+               Input box can be resized for better visualization by dragging the lower right corner.
+               When a sequence in FASTA format is input, the complementary output will also be in FASTA format."),
       width=3
     ),
     
